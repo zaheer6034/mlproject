18eb9775368f2869f76a543dab8d1b2a5e568d8b
Zaheer Abbas <zaheer6034@gmail.com>
2024-05-12 03:25:28 +0100

    pipeline for prediction

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,77 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+import os
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+
+    def predict(self, features):
+        
+        try:
+            
+            # model_path = 'artifact/model.pkl'
+            # preprocessor_path = 'artifact/preprocessor.pkl'
+            
+            model_path=os.path.join("artifact","model.pkl")
+            preprocessor_path=os.path.join('artifact','preprocessor.pkl')
+
+
+            model = load_object(file_path = model_path)
+            preprocessor = load_object(file_path = preprocessor_path)
+
+            print("Path Loaded successfully")
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+class CustomData:
+    def __init__(self, 
+                 gender,
+                 race_ethnicity,
+                 parental_level_of_education,
+                 lunch,
+                 test_preparation_course,
+                 reading_score,
+                 writing_score):
+        self.gender = gender
+        self.race_ethnicity = race_ethnicity,
+        self.parental_level_of_education = parental_level_of_education,
+        self.lunch = lunch,
+        self.test_preparation_course = test_preparation_course,
+        self.reading_score = reading_score,
+        self.writing_score = writing_score,
+
+
+    def get_data_as_frame(self):
+        try:
+            customer_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
+            }
+
+            # return pd.DataFrame(customer_data_input_dict)
+            df = pd.DataFrame(customer_data_input_dict)
+
+            # Remove tuples from DataFrame
+            df = df.applymap(lambda x: x[0] if isinstance(x, tuple) else x)
+
+            return df
+
+
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+
